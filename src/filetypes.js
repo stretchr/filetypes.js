e0078faec4a09213f7c0d9c7de26e2eb54df77a0
Mat Ryer <mat@stretchr.com>
2014-02-24 19:01:18 -0700

    initial commit

---
+++ src/filetypes.js
@@ -0,0 +1,84 @@
+/*
+
+
+
+  Usage:
+
+    Stretchr.Filetypes.descriptionFor("txt")
+    //= "Text file"
+
+    Stretchr.Filetypes.descriptionFor("text/plain");
+    //= "Text file"
+
+    Stretchr.Filetypes.mimeFor("txt")
+    //= "text/plain"
+
+*/
+
+(function(global){
+
+  global.Stretchr = global.Stretchr || {};
+  global.Stretchr._filetypes = [
+
+    /*
+      Here are the file types.
+
+        ext = array of extensions that match
+        mime = mime type if known (otherwise application/type will be assumned)
+        desc = (optional) description if "TYPE file" isn't enough
+
+    */
+
+    // text
+    {"ext": ["txt"], "mime":["text/plain"], "desc": "Text file"},
+    {"ext": ["xml"], "mime":["text/xml"]},
+
+    // video
+    {"ext": ["mov"],  "mime":["video/quicktime"],   "desc": "QuickTime Video file"},
+    {"ext": ["mp4"],  "mime":["video/mp4"],         "desc": "MPEG-4 Video file"},
+    {"ext": ["m3u8"], "mime":["video/MP2T"],        "desc": "iPhone Segment Video file"},
+    {"ext": ["ts"],   "mime":["video/quicktime"],   "desc": "QuickTime Video file"},
+    {"ext": ["3gp"],  "mime":["video/3gpp"],        "desc": "3GP Mobile Video file"},
+    {"ext": ["avi"],  "mime":["video/x-msvideo"],    "desc": "A/V Interleave Video file"},
+    {"ext": ["wmv"],  "mime":["video/x-ms-wmv"],     "desc": "Windows Media"},
+
+  ];
+  global.Stretchr.Filetypes = {
+    // find finds all matches for the specified extension or mime type.
+    find: function(s) {
+      s = s.toLowerCase();
+      var matches = [];
+      for (var i in global.Stretchr._filetypes) {
+        var item = global.Stretchr._filetypes[i];
+        if (item.ext.join(",").indexOf(s) > -1 || item.mime.join(",").indexOf(s) > -1) {
+          matches.push(item);
+        }
+      }
+      return matches;
+    },
+
+    descriptionFor: function(s) {
+      var match = this.find(s)[0];
+      if (match) {
+        if (match.desc) {
+          return match.desc;
+        } else {
+          return match.ext[0].toUpperCase() + " file";
+        }
+      }
+      return "Unknown file";
+    },
+
+    mimeFor: function(s) {
+      var match = this.find(s)[0];
+      if (match) {
+        if (match.mime && match.mime[0]) {
+          return match.mime[0];
+        }
+      }
+      return "application/" + s;
+    }
+
+  };
+
+})(window);
---
+++ test/spec/filetypes.js
@@ -0,0 +1,50 @@
+describe("Filetypes.js", function(){
+
+  it("should get the matches", function(){
+
+    var matches = Stretchr.Filetypes.find("txt");
+    expect(matches).not.toBeNull();
+    expect(matches.length).toBe(1);
+    expect(matches[0].mime, "text/plain");
+
+    var matches = Stretchr.Filetypes.find("xml");
+    expect(matches).not.toBeNull();
+    expect(matches.length).toBe(1);
+    expect(matches[0].mime, "text/xml");
+
+    var matches = Stretchr.Filetypes.find("text/plain");
+    expect(matches).not.toBeNull();
+    expect(matches.length).toBe(1);
+    expect(matches[0].mime, "text/plain");
+
+    var matches = Stretchr.Filetypes.find("text/xml");
+    expect(matches).not.toBeNull();
+    expect(matches.length).toBe(1);
+    expect(matches[0].mime, "text/xml");
+
+  });
+
+  it("should give you a description", function(){
+
+    // unknown
+    expect(Stretchr.Filetypes.descriptionFor("text/bollocks")).toBe("Unknown file")
+
+    // known
+    expect(Stretchr.Filetypes.descriptionFor("text/plain")).toBe("Text file")
+
+    // default
+    expect(Stretchr.Filetypes.descriptionFor("text/xml")).toBe("XML file")
+
+  });
+
+  it("should give you a mime type", function(){
+
+    // known
+    expect(Stretchr.Filetypes.mimeFor("txt")).toBe("text/plain");
+
+    // default
+    expect(Stretchr.Filetypes.mimeFor("monkey")).toBe("application/monkey");
+
+  });
+
+});
